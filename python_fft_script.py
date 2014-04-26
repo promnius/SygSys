a0e73f211c418a563d464c2369c53f238db5844a
Neal S <Neal@students.olin.edu>
2014-04-25 22:33:31 -0400

    add more to python fft script

--- python_fft_script.py
+++ python_fft_script.py
@@ -1,4 +1,6 @@
+import math
 
+pi = 3.14159
 
 arr = range(16)
 
@@ -13,3 +15,24 @@ def decompose(arr):
 	return right + left 
 
 print decompose(arr)
+
+def recompose(arr, current_level):
+    
+    
+    number_of_levels = math.log(arr, 2)
+    
+    for current_level in range(0, number_of_levels):    
+    
+        samples_in_level = current_level**2
+    
+        sinusoid_real = math.cos(pi / ((samples_in_level) / 2)  )
+        sinusoid_im = math.sin(pi / ((samples_in_level) / 2)  )
+        
+        for sample in range(0, (samples_in_level - 1) / 2):
+
+
+
+
+
+            
+
